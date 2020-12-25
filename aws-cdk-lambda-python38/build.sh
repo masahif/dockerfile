30fb29ac6b6a072d0e1949d0d7095861a47bc7ad
Masahiro Fukuda <masa@bodygram.com>
2020-12-25 06:02:04 +0000

    add python aws-cdk libraries change build process

--- aws-cdk-lambda-python38/build.sh
+++ aws-cdk-lambda-python38/build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
 set -eu
 
 if [ -z ${CDK_VERSION+x} ];
 then
-    echo "Must set CDK_VERSION to run this script."
-    exit -1;
+  CDK_VERSION=$(cat CDK_VERSION)
 else
-    echo "CDK VERSION: $CDK_VERSION";
+  echo "CDK VERSION: $CDK_VERSION"
 fi
-
-docker build -f Dockerfile -t masahif/aws-cdk-lambda-python3.8:$CDK_VERSION --build-arg CDK_VERSION=$CDK_VERSION .
+docker build -f Dockerfile -t aws-cdk-lambda-python3.8:$CDK_VERSION --build-arg CDK_VERSION=$CDK_VERSION .

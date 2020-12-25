19b058195c19c87104c582a68379ccd1a0cbc279
Masahiro Fukuda <masa@bodygram.com>
2020-12-25 08:14:14 +0000

    CDK 1.77.0

--- aws-cdk-lambda-python38/README.md
+++
@@ -1,5 +0,0 @@
-# AWS CDK for Lambda with Python3.8
-
-## how to build
-CDK_VERSION=1.78.0 ./build.sh
-
--- aws-cdk-lambda-python38/release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -eu
-CDK_VERSION=$(cat CDK_VERSION)
-git tag -a ${CDK_VERSION} -m "CDK version: ${CDK_VERSION}"
-git push origin --tags
